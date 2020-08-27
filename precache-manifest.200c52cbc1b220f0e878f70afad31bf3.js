966563247161aefad55257e919831477192b7523
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-08-26 21:04:47 -0500

    Updates

---
+++ precache-manifest.200c52cbc1b220f0e878f70afad31bf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f835d06df9e37ee379564f5f0a383e",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "84ea9b75c54f9e0d022a",
+    "url": "/User-Directory/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "df9700941eec4575b1fe",
+    "url": "/User-Directory/static/css/main.29f33316.chunk.css"
+  },
+  {
+    "revision": "84ea9b75c54f9e0d022a",
+    "url": "/User-Directory/static/js/2.394b7c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.394b7c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9700941eec4575b1fe",
+    "url": "/User-Directory/static/js/main.d851a2e7.chunk.js"
+  },
+  {
+    "revision": "e329bf187cf492e09831",
+    "url": "/User-Directory/static/js/runtime-main.627358b1.js"
+  }
+]);
